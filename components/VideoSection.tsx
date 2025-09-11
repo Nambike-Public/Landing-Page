2285039417776db5a5a12f74130d5099991a9d8f
anu1097 <anuraagg.kval3@gmail.com>
2025-09-12 03:46:17 +0530

    feat: updated YT video link and updated responsiveness

--- components/VideoSection.tsx
+++ components/VideoSection.tsx
@@ -1,12 +1,12 @@
 export default function VideoSection() {
 	return (
-		<section className="flex justify-center py-2 px-8 bg-[#0d1117]">
-			<div className="w-[90vw] h-[70vh]">
-				<div className="relative w-full h-full rounded-xl overflow-hidden shadow-2xl border border-blue-500/20">
+		<section className="flex justify-center py-2 px-4 sm:px-8 bg-[#0d1117]">
+			<div className="w-full">
+				<div className="relative w-full aspect-video rounded-xl overflow-hidden shadow-2xl border border-blue-500/20">
 					<iframe
 						width="100%"
 						height="100%"
-						src="https://www.youtube.com/embed/COg309fUsDM?si=34CkA4uVaD3Mz5Mc"
+						src="https://www.youtube.com/embed/q8fiCT8mfrY?si=_g7tO3962cMwyNXK"
 						title="YouTube video player"
 						frameBorder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
